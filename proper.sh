0a0a7b9a5096f69591c6a2f7f456482d26c038b3
kotaro.harada <kohtaro.harada@nts.ricoh.co.jp>
2014-07-29 17:53:42 +0900

    Feedback the result of ShellCheck.

--- proper.sh
+++ proper.sh
@@ -3,12 +3,12 @@
 proper()
 {
     len=${#1}
-    if [ ${len} -ge 1 ]
+    if [ "${len}" -ge 1 ]
     then
-        head=`echo ${1} | cut -c 1 | tr "[a-z]" "[A-Z]"`
-        if [ ${len} -ge 2 ]
+        head="$(echo "$1" | cut -c 1 | tr "[:lower:]" "[:upper:]")"
+        if [ "${len}" -ge 2 ]
         then
-            body=`echo ${1} | cut -c 2-${len} | tr "[A-Z]" "[a-z]"`
+            body="$(echo "$1" | cut -c 2-"${len}" | tr "[:upper:]" "[:lower:]")"
             result=${head}${body}
         else
             result=${head}
@@ -20,5 +20,4 @@ proper()
     return
 }
 
-echo `proper "AbCdEfG"`
-
+proper "AbCdEfG"
